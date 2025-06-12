0d5262e9eb7e798a2795506f264c00bef38c4333
IBRAHGITE <gite.ibrahim02@gmail.com>
2025-06-12 22:20:16 +0000

    navbar responsive

--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild ,ElementRef} from '@angular/core';
 import { Data } from '../../models/data.model';
 import { NgFor } from '@angular/common';
+import { Collapse } from 'bootstrap';
+import { HostListener } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   imports: [NgFor],
@@ -8,6 +10,7 @@ import { NgFor } from '@angular/common';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  @ViewChild('navbarCollapseRef') navbarCollapse!: ElementRef;
   navbarData = Data.navbar;
 
   constructor() {
@@ -15,5 +18,28 @@ export class NavbarComponent {
   toggleDarkMode() {
   document.body.classList.toggle('dark-mode');
 }
+closeMenu() {
+  if (window.innerWidth < 992) {
+    const bsCollapse = Collapse.getInstance(this.navbarCollapse.nativeElement) ||
+    new Collapse(this.navbarCollapse.nativeElement, { toggle: false });
+    bsCollapse.hide();
+  }
+}
+@HostListener('document:click', ['$event'])
+onClickOutside(event: MouseEvent) {
+  const targetElement = event.target as HTMLElement;
+
+  if (
+    this.navbarCollapse &&
+    !this.navbarCollapse.nativeElement.contains(targetElement) &&
+    !targetElement.classList.contains('navbar-toggler')
+  ) {
+    const collapse = Collapse.getInstance(this.navbarCollapse.nativeElement);
+    if (collapse && window.innerWidth < 992) {
+      collapse.hide();
+    }
+  }
+}
+
 
 }
